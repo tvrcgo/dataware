8b8da506152435aa5894aa4aa6f331adaafb16d8
tvrcgo <tvrcgo@gmail.com>
2017-06-30 19:30:52 +0800

    generator fn in test.

--- test/bucket.test.js
+++ test/bucket.test.js
@@ -10,91 +10,75 @@ describe('bucket', function() {
   const bucket = dw.bucket('test/testbucket')
   let tmpId = ''
 
-  it('post object', (done) => {
-    bucket.body({ title: 'hello' }).post().then(ret => {
-      assert(ret.story.title = 'hello')
-      tmpId = ret.story._id
-      done()
-    })
+  it('post object', function* () {
+    const ret = yield bucket.body({ title: 'hello' }).post()
+    assert(ret.story.title = 'hello')
   })
 
-  it('batch objects', (done) => {
-    bucket.body([
+  it('batch objects', function* () {
+    const ret = yield bucket.body([
       { title: 't1' },
       { title: 't2' },
       { title: 't3' }
-    ]).batch().then(ret => {
-      assert(ret.stories.length === 3)
-      assert(ret.meta.ok === 1)
-      assert(ret.meta.n === 3)
-      done()
-    })
+    ]).batch()
+    assert(ret.stories.length === 3)
+    assert(ret.meta.ok === 1)
+    assert(ret.meta.n === 3)
   })
 
-  it('query objects', (done) => {
-    bucket.query({
+  it('query objects', function* () {
+    const ret = yield bucket.query({
       where: {
         title: { $in: ['t1', 't2', 't3'] }
       },
       limit: 3
-    }).then(ret => {
-      assert(ret.length === 3)
-      done()
     })
+    assert(ret.length === 3)
   })
 
-  it('get object', (done) => {
-    bucket.get(tmpId).then(ret => {
-      assert(ret.title === 'hello')
-      assert(ret._id.toString() === tmpId.toString())
-      done()
-    })
+  it('get object', function* () {
+    const object = yield bucket.get(tmpId)
+    assert(ret.title === 'hello')
+    assert(ret._id.toString() === tmpId.toString())
   })
 
-  it('patch object', (done) => {
-    bucket.body({ title: 'abc', desc: 'def' }).post().then(ret => {
-      assert(ret.story.title === 'abc')
-      assert(ret.story.desc === 'def')
-      const storyId = ret.story._id
-      bucket.body({ title: 'def' }).patch(storyId).then(updret => {
-        assert(updret.meta.ok === 1)
-        assert(updret.meta.nModified === 1)
-        assert(updret.meta.n === 1)
-        bucket.get(storyId).then(ret3 => {
-          assert(ret3.title === 'def')
-          assert(ret3.desc === 'def')
-          done()
-        })
-      })
-    })
+  it('patch object', function* () {
+    const ret = yield bucket.body({ title: 'abc', desc: 'def' }).post()
+    assert(ret.story.title === 'abc')
+    assert(ret.story.desc === 'def')
+
+    const storyId = ret.story._id
+    const ret2 = yield bucket.body({ title: 'def' }).patch(storyId)
+    assert(ret2.meta.ok === 1)
+    assert(ret2.meta.nModified === 1)
+    assert(ret2.meta.n === 1)
+
+    const object = yield bucket.get(storyId)
+    assert(object.title === 'def')
+    assert(object.desc === 'def')
   })
 
-  it('put object', (done) => {
-    bucket.body({ title: 'abc', desc: 'def' }).post().then(ret => {
-      assert(ret.story.title === 'abc')
-      assert(ret.story.desc === 'def')
-      const storyId = ret.story._id
-      bucket.body({ title: 'def' }).put(storyId).then(updret => {
-        assert(updret.meta.ok === 1)
-        assert(updret.meta.nModified === 1)
-        assert(updret.meta.n === 1)
-        bucket.get(storyId).then(ret3 => {
-          assert(ret3.title === 'def')
-          assert(ret3.desc === undefined)
-          done()
-        })
-      })
-    })
+  it('put object', function* () {
+    const ret1 = yield bucket.body({ title: 'abc', desc: 'def' }).post()
+    assert(ret1.story.title === 'abc')
+    assert(ret1.story.desc === 'def')
+
+    const storyId = ret1.story._id
+    const ret2 = bucket.body({ title: 'def' }).put(storyId)
+    assert(ret2.meta.ok === 1)
+    assert(ret2.meta.nModified === 1)
+    assert(ret2.meta.n === 1)
+
+    const object = yield bucket.get(storyId)
+    assert(object.title === 'def')
+    assert(object.desc === undefined)
   })
 
-  it('remove object', (done) => {
-    bucket.body({ title: 'story title' }).post('test-story').then(ret => {
-      bucket.remove('test-story').then(rmret => {
-        assert(rmret.meta.ok === 1)
-        assert(rmret.meta.n === 1)
-        done()
-      })
-    })
+  it('remove object', function* () {
+    yield bucket.body({ title: 'story title' }).post('test-story')
+    const ret = yield bucket.remove('test-story')
+    assert(rmret.meta.ok === 1)
+    assert(rmret.meta.n === 1)
   })
 
   it('close dataware', () => {
