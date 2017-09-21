65486a7349c0c4ea79e3a4d8a33d0bf951d589ea
tvrcgo <tvrcgo@gmail.com>
2017-09-21 11:24:39 +0800

    test: modify by query.

--- test/bucket.test.js
+++ test/bucket.test.js
@@ -89,11 +89,39 @@ describe('bucket', function() {
     assert(ret3.data.desc === undefined)
   })
 
-  it('remove object', function* () {
+  it('put object by query', function* (){
+    const ret = yield bucket.body({ title: 'abc', type: 'put' }).post()
+    assert(ret.data.title === 'abc')
+
+    const ret2 = yield bucket.body({ title: 'abcd', type: 'putquery' }).put({
+      type: {
+        $eq: 'put'
+      }
+    })
+    assert(ret2.meta.ok === 1)
+    assert(ret2.meta.nModified === 1)
+
+    const ret3 = yield bucket.get(ret.data._id)
+    assert(ret3.data.title === 'abcd')
+    assert(ret3.data.type === 'putquery')
+  })
+
+  it('remove object by id', function* () {
     yield bucket.body({ title: 'story title' }).post('test-story')
     const ret = yield bucket.remove('test-story')
     assert(ret.meta.ok === 1)
     assert(ret.meta.n === 1)
+  })
+
+  it('remove object by query', function* (){
+    yield bucket.body([{ title: 'a' }, { title: 'a' }]).batch()
+    const ret = yield bucket.remove({
+      title: {
+        $eq: 'a'
+      }
+    })
+    assert(ret.meta.ok === 1)
+    assert(ret.meta.n === 2)
   })
 
   it('other bucket', function* () {
