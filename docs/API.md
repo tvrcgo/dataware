facde3515aed8e809ad025f42972d027c47d0ac3
tvrcgo <tvrcgo@gmail.com>
2019-11-22 13:38:21 +0800

    change object() to use()

--- docs/API.md
+++ docs/API.md
@@ -8,27 +8,28 @@ import { Bucket } from 'dataware'
 const bucket = new Bucket({
   auth: 'username:password',
   authSource: 'admin',
-  endpoint: 'localhost:27017'
+  endpoint: 'localhost:27017',
+  database: 'db'
 })
 ```
 
 Create
 ```js
 // post object
-bucket.object('db/collection').body(object).post()
-bucket.object('db/collection').body(object).post('object-id')
+bucket.use('collection').body(object).post()
+bucket.use('collection').body(object).post('object-id')
 
 // post batch objects
-bucket.object('db/collection').body([ object1, object2, object3 ]).post()
+bucket.use('collection').body([ object1, object2, object3 ]).post()
 ```
 
 Read
 ```js
 // get object by id
-bucket.object('db/collection').get('object-id')
+bucket.use('collection').get('object-id')
 
-// query object(s)
-bucket.object('db/collection').query({
+// query use(s)
+bucket.use('collection').query({
   where: {
     title: {
       $in: ['t1', 't2', 't3']
@@ -45,22 +46,22 @@ bucket.object('db/collection').query({
 Update
 ```js
 // update object by id
-bucket.object('db/collection').body(patchBody).patch('object-id')
-bucket.object('db/collection').body(newBody).put('object-id')
+bucket.use('collection').body(patchBody).patch('object-id')
+bucket.use('collection').body(newBody).put('object-id')
 
-// update object(s) by query
-bucket.object('db/collection').body(patchBody).patch({ type: { $eq: 1 } })
-bucket.object('db/collection').body(newBody).put({ name: { $eq: 'aaa' }})
+// update use(s) by query
+bucket.use('collection').body(patchBody).patch({ type: { $eq: 1 } })
+bucket.use('collection').body(newBody).put({ name: { $eq: 'aaa' }})
 ```
 
 Delete
 ```js
 // remove object by id
-bucket.object('db/collection').remove('object-id')
+bucket.use('collection').remove('object-id')
 
-// remove object(s) by query
-bucket.object('db/collection').remove({ number: { $gt: 5 }})
+// remove use(s) by query
+bucket.use('collection').remove({ number: { $gt: 5 }})
 
 // drop bucket
-bucket.object('db/collection').drop()
+bucket.use('collection').drop()
 ```
--- test/bucket.test.js
+++ test/bucket.test.js
@@ -5,19 +5,20 @@ const assert = require('power-assert')
 describe('bucket', () => {
 
   const bucket = new Bucket({
-    endpoint: 'localhost:27017'
+    endpoint: 'localhost:27017',
+    database: 'test'
   })
 
   let tmpId = ''
 
   it('post object', async () => {
-    const ret = await bucket.object('test/testbucket').body({ title: 'hello' }).post()
+    const ret = await bucket.use('testbucket').body({ title: 'hello' }).post()
     assert(ret.data.title = 'hello')
     tmpId = ret.data._id
   })
 
   it('post batch objects', async () => {
-    const ret = await bucket.object('test/testbucket').body([
+    const ret = await bucket.use('testbucket').body([
       { title: 't1' },
       { title: 't2' },
       { title: 't3' }
@@ -28,7 +29,7 @@ describe('bucket', () => {
   })
 
   it('query objects', async () => {
-    const ret = await bucket.object('test/testbucket').query({
+    const ret = await bucket.use('testbucket').query({
       where: {
         title: { $in: ['t1', 't2', 't3'] }
       },
@@ -38,33 +39,33 @@ describe('bucket', () => {
   })
 
   it('get object', async () => {
-    const ret = await bucket.object('test/testbucket').get(tmpId)
+    const ret = await bucket.use('testbucket').get(tmpId)
     assert(ret.data.title === 'hello')
     assert(ret.data._id.toString() === tmpId.toString())
   })
 
   it('patch object by id', async () => {
-    const ret = await bucket.object('test/testbucket').body({ title: 'abc', desc: 'def' }).post()
+    const ret = await bucket.use('testbucket').body({ title: 'abc', desc: 'def' }).post()
     assert(ret.data.title === 'abc')
     assert(ret.data.desc === 'def')
 
     const storyId = ret.data._id
-    const ret2 = await bucket.object('test/testbucket').body({ title: 'def' }).patch(storyId)
+    const ret2 = await bucket.use('testbucket').body({ title: 'def' }).patch(storyId)
     assert(ret2.meta.ok === 1)
     assert(ret2.meta.nModified === 1)
     assert(ret2.meta.n === 1)
 
-    const ret3 = await bucket.object('test/testbucket').get(storyId)
+    const ret3 = await bucket.use('testbucket').get(storyId)
     assert(ret3.data.title === 'def')
     assert(ret3.data.desc === 'def')
   })
 
   it('patch object by query', async () => {
-    const ret1 = await bucket.object('test/testbucket').body({ title: 'a', type: 1 }).post()
-    const ret2 = await bucket.object('test/testbucket').body({ title: 'b', type: 1 }).post()
+    const ret1 = await bucket.use('testbucket').body({ title: 'a', type: 1 }).post()
+    const ret2 = await bucket.use('testbucket').body({ title: 'b', type: 1 }).post()
     assert(ret1.data.title === 'a')
     assert(ret2.data.title === 'b')
-    const ret3 = await bucket.object('test/testbucket').body({ title: 'c' }).patch({
+    const ret3 = await bucket.use('testbucket').body({ title: 'c' }).patch({
       type: {
         $eq: 1
       }
@@ -74,26 +75,26 @@ describe('bucket', () => {
   })
 
   it('put object by id', async () => {
-    const ret1 = await bucket.object('test/testbucket').body({ title: 'abc', desc: 'def' }).post()
+    const ret1 = await bucket.use('testbucket').body({ title: 'abc', desc: 'def' }).post()
     assert(ret1.data.title === 'abc')
     assert(ret1.data.desc === 'def')
 
     const storyId = ret1.data._id
-    const ret2 = await bucket.object('test/testbucket').body({ title: 'def' }).put(storyId)
+    const ret2 = await bucket.use('testbucket').body({ title: 'def' }).put(storyId)
     assert(ret2.meta.ok === 1)
     assert(ret2.meta.nModified === 1)
     assert(ret2.meta.n === 1)
 
-    const ret3 = await bucket.object('test/testbucket').get(storyId)
+    const ret3 = await bucket.use('testbucket').get(storyId)
     assert(ret3.data.title === 'def')
     assert(ret3.data.desc === undefined)
   })
 
   it('put object by query', async () => {
-    const ret = await bucket.object('test/testbucket').body({ title: 'abc', type: 'put' }).post()
+    const ret = await bucket.use('testbucket').body({ title: 'abc', type: 'put' }).post()
     assert(ret.data.title === 'abc')
 
-    const ret2 = await bucket.object('test/testbucket').body({ title: 'abcd', type: 'putquery' }).put({
+    const ret2 = await bucket.use('testbucket').body({ title: 'abcd', type: 'putquery' }).put({
       type: {
         $eq: 'put'
       }
@@ -101,21 +102,21 @@ describe('bucket', () => {
     assert(ret2.meta.ok === 1)
     assert(ret2.meta.nModified === 1)
 
-    const ret3 = await bucket.object('test/testbucket').get(ret.data._id)
+    const ret3 = await bucket.use('testbucket').get(ret.data._id)
     assert(ret3.data.title === 'abcd')
     assert(ret3.data.type === 'putquery')
   })
 
   it('remove object by id', async () => {
-    await bucket.object('test/testbucket').body({ title: 'story title' }).post('test-story')
-    const ret = await bucket.object('test/testbucket').remove('test-story')
+    await bucket.use('testbucket').body({ title: 'story title' }).post('test-story')
+    const ret = await bucket.use('testbucket').remove('test-story')
     assert(ret.meta.ok === 1)
     assert(ret.meta.n === 1)
   })
 
   it('remove object by query', async () => {
-    await bucket.object('test/testbucket').body([{ title: 'a' }, { title: 'a' }]).post()
-    const ret = await bucket.object('test/testbucket').remove({
+    await bucket.use('testbucket').body([{ title: 'a' }, { title: 'a' }]).post()
+    const ret = await bucket.use('testbucket').remove({
       title: {
         $eq: 'a'
       }
@@ -125,14 +126,14 @@ describe('bucket', () => {
   })
 
   it('other collection', async () => {
-    const obj = await bucket.object('test/testbucket2').body({ title: 'other' }).post()
+    const obj = await bucket.use('testbucket2').body({ title: 'other' }).post()
     assert(obj.data.title === 'other')
     assert(obj.meta.ok === 1)
     assert(obj.meta.n === 1)
   })
 
   it('drop collection', async () => {
-    const ret = await bucket.object('test/testbucket').drop()
+    const ret = await bucket.use('testbucket').drop()
     assert(ret === true)
   })
 
